29c47d41dbd2c087bda8c8a8a056b202a169a411
Torystan <tristan.lp@live.fr>
2023-02-07 12:36:16 +0100

    mesure timestamp

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import os
 import cv2  # OpenCV library
 import matplotlib.pyplot as plt
 from scipy.ndimage import uniform_filter1d
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import time
+import pandas as pd
+
 
 from analyzers.analyseDerive import AnalyseDerive
 from analyzers.analyseSafran import AnalyseSafran
 from analyzers.analyseMousse import AnalyseMousse
 from analyzers.dataRecovery import DataRecovery
+
 
 
 class Main():
@@ -26,10 +32,15 @@ class Main():
         Constructeur de la class Main()
         """
 
-        self.cap = cv2.VideoCapture(os.path.dirname(__file__) + "/video/ccc2.mp4")
+        #self.cap = cv2.VideoCapture(os.path.dirname(__file__) + "/video/ccc2.mp4")
+        self.cap = cv2.VideoCapture("rtsp://root:M101_svr@192.168.1.55:554/axis-media/media.amp") # 192.168.1.55 ou 192.168.1.56
+        #self.cap2 = cv2.VideoCapture("rtsp://root:M101_svr@192.168.1.56:554/axis-media/media.amp") # 192.168.1.55 ou 192.168.1.56
         self.record = False
         self.videoObject = None
         self.nbFrame = 1
+
+        self.times = []
+        self.times2 = []
 
         self.analyses = {}
         self.analyses["derive"] = AnalyseDerive(1400, 250, 1514, 417)
@@ -44,19 +55,20 @@ class Main():
             frame_height = int(self.cap.get(4))
 
             size = (frame_width, frame_height)
-            self.videoObject = cv2.VideoWriter(os.path.dirname(__file__) + "/video/record.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 30, size)
+            self.videoObject = cv2.VideoWriter(os.path.dirname(__file__) + "/video/record5.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 25, size)
 
     def execute(self):
         """
         Fonction d'éxécution du programme principal de la lecture de la vidéo à l'affichage des données.
         """
 
-        while True:
+        while self.nbFrame < 200:
 
             ###### Lecture de la vidéo ######
 
             # Récupère une image de la vidéo
             ret, frame = self.cap.read()
+            self.times.append(time.time_ns())
             if frame is None:
                 print("-----  Fin de la vidéo  -----")
                 break
@@ -69,6 +81,8 @@ class Main():
 
             ###### Dessin des données #####
 
+            cv2.line(frame, (411, 437), (445, 630), (125, 125, 125), 1)
+
             for aMeasureKey, aMeasureValue in self.dataRecovery.data.items():
                 if aMeasureValue["height"][-1] != None:  # Pas de dessin des mesures vides
                     
@@ -79,6 +93,7 @@ class Main():
 
                     cv2.drawContours(frame, [aMeasureValue["contour"][-1]], 0, (255, 0, 255), 1)
                     cv2.line(frame, aMeasureValue["firstPosMeasure"][-1], aMeasureValue["secondPosMeasure"][-1], color, 2)
+
 
             cv2.putText(frame, str(self.nbFrame), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 2, cv2.LINE_AA)
             cv2.putText(frame, "Erreur : ", (10, 110), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
@@ -100,7 +115,14 @@ class Main():
 
             self.nbFrame += 1
 
+            self.times2.append(time.time_ns())
+
         ###### Fin de l'analyse de la vidéo #####
+
+        newDataframe = pd.DataFrame()
+        newDataframe["time"] = self.times
+        newDataframe["time2"] = self.times2
+        newDataframe.to_excel(os.path.dirname(__file__) + "/files/outputExcelFile.xlsx", index = False)
 
         self.cap.release()
 
@@ -114,11 +136,14 @@ class Main():
 
         self.dataRecovery.convertToDataframe()
 
-        fig, axs = plt.subplots(2)
-        fig.suptitle('Hauteur en pixels en fonction du temps avc courbe de qualité')
+        fig = make_subplots(rows=2, cols=1)
 
         for aMeasureKey in self.dataRecovery.data:
-            axs[1].plot(self.dataRecovery.data[aMeasureKey]["numFrame"].values.tolist(), self.dataRecovery.data[aMeasureKey]["quality"], label=aMeasureKey)
+            fig.append_trace(go.Scatter(
+            x=self.dataRecovery.data[aMeasureKey]["numFrame"].values.tolist(),
+            y=self.dataRecovery.data[aMeasureKey]["quality"],
+            name="qualité_"+aMeasureKey
+            ), row=2, col=1)
 
         for aMeasureKey in self.dataRecovery.data:
             # On garde uniquement les données de bonne qualité
@@ -129,13 +154,17 @@ class Main():
             self.dataRecovery.data[aMeasureKey]["height"] = uniform_filter1d(self.dataRecovery.data[aMeasureKey]["height"].values.tolist(), size=1)
 
         ###### Affichage des résultats ######
-
+        
         # Courbes
         for aMeasureKey in self.dataRecovery.data:
-            axs[0].plot(self.dataRecovery.data[aMeasureKey]["numFrame"].values.tolist(), self.dataRecovery.data[aMeasureKey]["height"], label=aMeasureKey)
+            fig.append_trace(go.Scatter(
+                x=self.dataRecovery.data[aMeasureKey]["numFrame"].values.tolist(),
+                y=self.dataRecovery.data[aMeasureKey]["height"],
+                name=aMeasureKey
+            ), row=1, col=1)
 
-        plt.legend()
-        plt.show()
+        fig.update_layout(title_text="Courbes des hauteurs et qualité de mesure")
+        fig.show()
 
 
 if __name__ == "__main__":
